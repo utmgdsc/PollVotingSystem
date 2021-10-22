aaa5d922ada1d1b886a7af57f833abd63b934dc4
Chris <hello@hiimchrislim.co>
2021-10-22 17:47:50 -0400

    Test githook

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,9 +5,8 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      fontFamily: {
-        sans: ["Raleway", ...defaultTheme.fontFamily.mono],
-      }
+      fontFamily: {sans: ["Raleway", ...defaultTheme.fontFamily.mono],
+      },
     },
     backgroundColor: (theme) => ({
       ...theme("colors"),
