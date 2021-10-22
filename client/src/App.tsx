c00595039fd2c0775c8e5e2ba76ae15bbbf87bc7
Chris <hello@hiimchrislim.co>
2021-10-22 17:37:15 -0400

    Add TailwindCSS setup and partial Navbar Progress

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Navbar, Option } from "./components/Navbar";
 
 const App = () => {
-  return <div>Test</div>;
+  const arr: Array<Option> = [
+    {
+      name: "Github",
+      href: "/github",
+    },
+    {
+      name: "Logout",
+      href: "/logout",
+    },
+  ];
+
+  return (
+    <div className={"min-h-screen bg-background"}>
+      <Navbar options={arr} />
+    </div>
+  );
 };
 
 export default App;
---
+++ client/tailwind.config.js
@@ -0,0 +1,22 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  purge: ["./src/**/*.{ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        "sans": ["Raleway", ...defaultTheme.fontFamily.mono]
+      }
+    },
+    backgroundColor: theme => ({
+      ...theme('colors'),
+      'primary': "#00204E",
+      'background': "#F5F5F5",
+    })
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+};
