5973379ed393ddc5f41d2e10e49c00f55e0a500c
Shubh Bapna <shubh.bapna@mail.utoronto.ca>
2021-11-03 13:50:13 -0400

    disconnect sockets when the poll closes

--- server/src/controllers/pollController.ts
+++ server/src/controllers/pollController.ts
@@ -97,7 +97,7 @@ async function endForever(pollId: string) {
   if (pollId === null || pollId === undefined)
     return { status: 400, data: { message: "Invalid poll Id" } };
   await client.del(pollId);
-  io.to(pollId).emit("close", { message: "Poll is over" });
+  io.of("/").in(pollId).disconnectSockets();
   return { status: 200, data: { message: "Poll closed" } };
 }
 
