346dc54a0d7ea37a9d8572a302f584925cc8da95
Shubh Bapna <shubh.bapna@mail.utoronto.ca>
2021-11-20 14:45:19 -0500

    testing shibboleth with sockets

--- server/src/server.ts
+++ server/src/server.ts
@@ -27,9 +27,29 @@ app.use(
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
+app.use((req, res, next) => {
+  if (req.headers.utorid != undefined) {
+    console.log(
+      "shib user " + req.headers.utorid + " " + req.headers.http_mail
+    );
+    console.log("origin " + req.headers.origin);
+  }
+  next();
+});
 app.use("/poll", pollRouter);
 
 const server = app.listen(port, () => {
   console.log("Listening on http://localhost:" + port);
   io.attach(server);
+  io.use((socket, next) => {
+    if (socket.handshake.headers.utorid != undefined) {
+      console.log(
+        "shib user " +
+          socket.handshake.headers.utorid +
+          " " +
+          socket.handshake.headers.http_mail
+      );
+      console.log("origin " + socket.handshake.headers.origin);
+    }
+  });
 });
