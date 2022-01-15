bb89c2ccc9d2445384ea74a72a7256d272bd049a
Chris <hello@hiimchrislim.co>
2022-01-14 23:33:18 -0500

    Add persistent background change on button select

--- client/src/components/PollOptionButton.tsx
+++ client/src/components/PollOptionButton.tsx
@@ -4,14 +4,22 @@ interface buttonType {
   name: string;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
   disabled?: boolean;
+  selected: boolean;
 }
 
-export const PollOptionButton = ({ name, onClick, disabled }: buttonType) => {
+export const PollOptionButton = ({
+  name,
+  onClick,
+  disabled,
+  selected,
+}: buttonType) => {
   return (
     <button
       disabled={disabled}
       onClick={onClick}
-      className={`m-2 py-2 px-40 inline-block bg-primary ${
+      className={`m-2 py-2 px-40 inline-block ${
+        selected ? "bg-hover" : "bg-primary"
+      } ${
         disabled
           ? "cursor-not-allowed opacity-50"
           : "hover:bg-hover cursor-pointer"
