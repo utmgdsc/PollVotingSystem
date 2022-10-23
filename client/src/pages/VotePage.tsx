19bfdcafb9a9472883ccd5dbb3410e4002cf69a7
Ido <benhaim.ido@gmail.com>
2022-10-23 19:14:20 -0400

    Centered spinner
    Thanks @embeddedt
    
    Co-authored-by: embeddedt <42941056+embeddedt@users.noreply.github.com>

--- client/src/pages/VotePage.tsx
+++ client/src/pages/VotePage.tsx
@@ -140,7 +140,7 @@ export const VotePage = () => {
               <div role="status">
                 <svg
                   aria-hidden="true"
-                  className="mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-900 fill-blue-600"
+                  className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-900 fill-blue-600"
                   viewBox="0 0 100 101"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
