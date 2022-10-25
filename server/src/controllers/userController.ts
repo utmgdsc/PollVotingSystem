ecb4c4ff2353ad6737bca497a7cdc546ff9a71f2
Ido <benhaim.ido@gmail.com>
2022-10-24 23:43:41 -0400

    Linting and Formatting for the backend (#71)
    * eslint setup (missing husky integration)
    
    #62
    
    * Fixed eslint errors
    
    * Added lint:fix script to package.json
    
    * added server pre-commit husky
    
    Co-authored-by: Shubh Bapna <shubh.bapna@mail.utoronto.ca>

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,11 +1,10 @@
-import { client } from "../redis";
+import { client } from '../redis'
 
-async function getUser(utorid: any) {
-  if (utorid === null || utorid === undefined || typeof utorid !== "string")
-    return { status: 400, data: { message: "Invalid utorid" } };
-  let userType = await client.get(utorid);
-  if (userType == null) userType = "student";
-  return { status: 200, data: { userType } };
+async function getUser (utorid: any) {
+  if (utorid === null || utorid === undefined || typeof utorid !== 'string') { return { status: 400, data: { message: 'Invalid utorid' } } }
+  let userType = await client.get(utorid)
+  if (userType == null) userType = 'student'
+  return { status: 200, data: { userType } }
 }
 
-export { getUser };
+export { getUser }
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,62 +1,62 @@
-"use strict";
+'use strict'
 
-import * as dotenv from "dotenv";
-dotenv.config();
-import express from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import { io } from "./socket";
-import pollRouter from "./routes/pollRoute";
-import { db } from "./db/mogoose";
-import userRouter from "./routes/userRoutes";
-import { getUser } from "./controllers/userController";
-db.on("open", () => {
-  console.log("Connected to mongo");
-});
+import * as dotenv from 'dotenv'
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import { io } from './socket'
+import pollRouter from './routes/pollRoute'
+import { db } from './db/mogoose'
+import userRouter from './routes/userRoutes'
+import { getUser } from './controllers/userController'
+dotenv.config()
+db.on('open', () => {
+  console.log('Connected to mongo')
+})
 
 // starting the express server
-const app = express();
-const port = process.env.PORT || 5000;
+const app = express()
+const port = process.env.PORT || 5000
 
 // parse cookies and body and enable cors
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
-    methods: "GET,POST,DELETE,PATCH",
+    methods: 'GET,POST,DELETE,PATCH'
   })
-);
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cookieParser());
-app.use("/user", userRouter);
+)
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cookieParser())
+app.use('/user', userRouter)
 app.use(async (req, res, next) => {
   try {
-    const userType = await getUser(req.headers.utorid);
-    if (userType.data.userType === "instructor") next();
-    else next(new Error("Forbidden User"));
+    const userType = await getUser(req.headers.utorid)
+    if (userType.data.userType === 'instructor') next()
+    else next(new Error('Forbidden User'))
   } catch (err) {
-    next(new Error("Forbidden User"));
+    next(new Error('Forbidden User'))
   }
-});
-app.use("/poll", pollRouter);
+})
+app.use('/poll', pollRouter)
 
 const server = app.listen(port, () => {
-  console.log("Listening on http://localhost:" + port);
-  io.attach(server);
+  console.log('Listening on http://localhost:' + port)
+  io.attach(server)
   io.use(async (socket, next) => {
     try {
-      if (socket.handshake.headers.utorid != undefined) {
-        socket.data["utorid"] = socket.handshake.headers.utorid;
-        next();
-        return;
-      } else if (socket.data.utorid != undefined) {
-        next();
-        return;
+      if (socket.handshake.headers.utorid) {
+        socket.data.utorid = socket.handshake.headers.utorid
+        next()
+        return
+      } else if (socket.data.utorid) {
+        next()
+        return
       }
-      next(new Error("Not Authorized"));
+      next(new Error('Not Authorized'))
     } catch (err) {
-      next(new Error("Not Authorized"));
+      next(new Error('Not Authorized'))
     }
-  });
-});
+  })
+})
